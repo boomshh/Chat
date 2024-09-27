d7531b3449855569829151ab37c9c3ced6a0decc
Ethan <fanfirst4@naver.com>
2024-09-27 21:43:24 +0900

    [UI] SignUp 페이지

---
+++ app/src/main/java/com/example/chata/screen/SignUpScreen.kt
@@ -0,0 +1,92 @@
+package com.example.chata.screen
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignUpScreen() {
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it},
+            label = { Text("Email")},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            label = { Text("Password")},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            visualTransformation = PasswordVisualTransformation())
+
+        OutlinedTextField(value = firstName,
+            onValueChange = {firstName = it},
+            label = { Text("FirstName")},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp))
+
+        OutlinedTextField(value = lastName,
+            onValueChange = {lastName = it},
+            label = { Text("LastName")},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
+
+        Button(
+            onClick = {
+                email = ""
+                password = ""
+                firstName = ""
+                lastName = ""
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text("Sign Up")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("Already have an account? Sign in.",
+            modifier = Modifier.clickable {  })
+    }
+}
