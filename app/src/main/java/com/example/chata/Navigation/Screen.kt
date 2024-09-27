363bddeb2fc84fa4575baa171418cd7969c7b973
Ethan <fanfirst4@naver.com>
2024-09-27 22:24:17 +0900

    .

--- app/src/main/java/com/example/chata/Navigation/Screen.kt
+++ app/src/main/java/com/example/chata/Navigation/Screen.kt
@@ -2,8 +2,11 @@ package com.example.chata.Navigation
 
 sealed class Screen(val route: String) {
 
+    // 페이지 간 경로 루트
     object LoginScreen: Screen("loginscreen")
     object SignupScreen: Screen("signupscreen")
     object ChatRoomsScreen:Screen("chatroomscreen")
     object ChatScreen:Screen("chatscreen")
+
+
 }
