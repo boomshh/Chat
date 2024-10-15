3a47887c0811386394b42e22e38891077008e5b8
Ethan <fanfirst4@naver.com>
2024-10-15 23:01:41 +0900

    [UI] chat dialog

--- app/src/main/java/com/example/chata/screen/ChatRoomListScreen.kt
+++ app/src/main/java/com/example/chata/screen/ChatRoomListScreen.kt
@@ -1,13 +1,18 @@
 package com.example.chata.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,6 +24,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChatRoomListScreen() {
     var showDialog by remember { mutableStateOf(false)}
@@ -44,6 +50,40 @@ fun ChatRoomListScreen() {
             modifier = Modifier.fillMaxWidth()
         ) {
             Text(text = "Create Room")
+        }
+        
+        if(showDialog) {
+            AlertDialog(onDismissRequest = { showDialog = true},
+                title = { Text(text = "Create a new room")},
+                text = {
+                    OutlinedTextField(value = name,
+                        onValueChange = {name = it},
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp))
+                }, confirmButton = {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Button(onClick = {
+                            if (name.isNotBlank()) {
+                                showDialog = false
+                            }
+                        }) {
+                            Text(text = "Add")
+                            
+                        }
+                        Button(
+                            onClick = { showDialog = false}
+                        ) {
+                            Text(text = "Cancel")
+                        }
+                    }
+                })
         }
         
         
