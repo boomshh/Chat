9c110a654fc16b9a4c00a0d93266e2ff3193ae99
Ethan <fanfirst4@naver.com>
2024-10-15 23:14:34 +0900

    [UI] RoomItem

---
+++ app/src/main/java/com/example/chata/screen/RoomItem.kt
@@ -0,0 +1,29 @@
+package com.example.chata.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.chata.Room
+
+@Composable
+fun RoomItem(room: Room) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = room.name, fontSize = 16.sp, fontWeight = FontWeight.Normal)
+        OutlinedButton(onClick = {}) {
+            Text(text = "Join")
+        }
+    }
+}
