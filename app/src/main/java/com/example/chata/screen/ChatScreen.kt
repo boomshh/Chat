362810efccbba2e423f5544349b72e0fe9f24712
Ethan <fanfirst4@naver.com>
2024-10-21 22:05:20 +0900

    [FEAT] 타임스탬프를 이용하여 각 메시지가 보내진 시간 표시

--- app/src/main/java/com/example/chata/screen/ChatScreen.kt
+++ app/src/main/java/com/example/chata/screen/ChatScreen.kt
@@ -1,5 +1,7 @@
 package com.example.chata.screen
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,6 +21,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun ChatScreen(roomId: String) {
@@ -62,4 +68,36 @@ fun ChatScreen(roomId: String) {
             }
         }
     }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun formatTimestamp(timestamp : Long) : String {
+    val messageDateTime =
+        LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp),
+            ZoneId.systemDefault())
+    val now = LocalDateTime.now()
+
+    return when {
+        isSameDay(messageDateTime, now) -> "today ${formatTime(messageDateTime)}"
+        isSameDay(messageDateTime.plusDays(1), now) -> "yesterday ${formatTime(messageDateTime)}"
+        else -> formatDate(messageDateTime)
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun isSameDay(dateTime1: LocalDateTime, dateTime2: LocalDateTime) : Boolean {
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    return dateTime1.format(formatter) == dateTime2.format(formatter)
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun formatTime(dateTime: LocalDateTime): String {
+    val formatter = DateTimeFormatter.ofPattern("HH:mm")
+    return formatter.format(dateTime)
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun formatDate(dateTime: LocalDateTime): String {
+    val formatter = DateTimeFormatter.ofPattern("MMM d, yyyy")
+    return formatter.format(dateTime)
 }
