0575f946f0aa5798d8e38550b6fcab7f30d57f7e
Ethan <fanfirst4@naver.com>
2024-10-21 22:39:09 +0900

    [UI] message UI

---
+++ app/src/main/java/com/example/chata/Message.kt
@@ -0,0 +1,10 @@
+package com.example.chata
+
+data class Message(
+    val senderFirstName: String = "",
+    val senderId:String = "",
+    val text: String = "",
+    val timestamp: Long = System.currentTimeMillis(),
+    val isSentByCurrentUser: Boolean = false
+
+)
--- app/src/main/java/com/example/chata/screen/ChatScreen.kt
+++ app/src/main/java/com/example/chata/screen/ChatScreen.kt
@@ -2,25 +2,35 @@ package com.example.chata.screen
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.chata.Message
+import com.example.chata.R
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -67,6 +77,45 @@ fun ChatScreen(roomId: String) {
 
             }
         }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ChatMessageItem(message: Message) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalAlignment = if(message.isSentByCurrentUser) Alignment.End else Alignment.Start
+    ) {
+        Box(
+            modifier = Modifier
+                .background(
+                    if (message.isSentByCurrentUser) colorResource(id = R.color.purple_700)
+                    else Color.Gray, shape = RoundedCornerShape(8.dp)
+                )
+                .padding(8.dp)
+        ) {
+            Text(
+                text = message.text,
+                color = Color.White,
+                style = TextStyle(fontSize = 16.sp)
+            )
+        }
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = message.senderFirstName,
+            style = TextStyle(
+                fontSize = 12.sp,
+                color = Color.Gray
+            )
+        )
+        Text(text = formatTimestamp(message.timestamp),
+            style = TextStyle(
+                fontSize = 12.sp,
+                color = Color.Gray
+            ))
     }
 }
 
